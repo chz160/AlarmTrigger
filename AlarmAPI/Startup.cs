3e6190d84bb8fc651187d4638e521e2afd2161e3
chz160 <noahporch@gmail.com>
2015-05-03 11:18:27 -0500

    Initial Checkin

---
+++ AlarmAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlarmAPI.Startup))]
+namespace AlarmAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
