37b797ee65e1474231b10ab2296615d48cb6dcfe
chz160 <noahporch@gmail.com>
2015-05-03 11:32:00 -0500

    Added AlarmFinder

---
+++ AlarmFinder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AlarmFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lower = 1;
+            var upper = 254;
+            var threadList = new List<Task>();
+            for (int i = 1; i <= upper; i++)
+            {
+                threadList.Add(
+                    Task.Factory.StartNew(() =>
+                    {
+                        using (var client = new WebClient())
+                        {
+                            var url = string.Format("http://10.10.1.{0}", i);
+                            var data = client.DownloadString(new Uri(url));
+                            
+                        }
+                    })
+                );
+            }
+            
+        }
+    }
+}
